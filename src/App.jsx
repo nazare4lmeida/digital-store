23422cf043cb6bbdd20cdda9cd7754fba217f3e2
Nazare Almeida <nazyalmeida@gmail.com>
2025-06-23 20:15:47 -0300

    Novas alterações projeto front end

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+import { Outlet } from 'react-router-dom';
+// Futuramente importaremos Header e Footer aqui
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <main>
+        <Outlet /> {/* Onde o conteúdo da página atual será renderizado */}
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default App;
